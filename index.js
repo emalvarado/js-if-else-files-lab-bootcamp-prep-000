08133083b42bf7ebf46ecff606444e026c7f00d7
ELIZABETH ALVARADO <lily21418@yahoo.com>
2018-04-12 05:31:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var height = "74"
 if(name === "susan") {
   console.log("The name is correct")
 }
+else(name === "Susan") {
+  console.log("Expected the name to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
